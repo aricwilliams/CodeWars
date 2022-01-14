1998a0864e4eb4cda4f1b88d0a1a442545b76ca1
Aric Williams <a.williams@interroll.com>
2022-01-14 15:06:45 -0500

    2 easy code challages

---
+++ program.cs
@@ -0,0 +1,37 @@
+﻿/*Create a function that accepts a string (of a person's first and last name) and returns a string with the first and last name swapped.
+
+Examples
+nameShuffle("Donald Trump") ➞ "Trump Donald"
+
+nameShuffle("Rosie O'Donnell") ➞ "O'Donnell Rosie"
+
+nameShuffle("Seymour Butts") ➞ "Butts Seymour"
+Notes
+There will be exactly one space between the first and last name.
+If you get stuck on a challenge, find help in the Resources tab.
+If you're really stuck, unlock solutions in the Solutions tab.
+
+
+
+public class program
+{
+	function nameShuffle(str)
+	{
+		return str.split(' ').reverse().join(' ');
+	}*/
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//Write a function that converts hours into seconds.
+
+//one hour = 3600 seconds because 60 seconds is one minute and i need 60 minutes to get 1 hour... so 60 * 60 =3600
+/*
+public class program
+{
+	function howManySeconds(hours)
+	{
+		return hours * 3600;
+	}
+
+}*/
