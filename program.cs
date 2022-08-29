8d0e12544e0b5bb24f091eb4f399b7df95fc4a18
aric williams <aricwilliamst@gmail.com>
2022-08-29 15:55:24 -0400

    More questions

--- program.cs
+++ program.cs
@@ -173,9 +173,62 @@ public class program
 //     }
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////
+// Sentence Smash
+// Write a function that takes an array of words and smashes them together into a sentence and returns the sentence. You can ignore any need to sanitize words or add punctuation, but you should add spaces between each word. Be careful, there shouldn't be a space at the beginning or the end of the sentence!
+
+// Example
+// ['hello', 'world', 'this', 'is', 'great']  =>  'hello world this is great'
 
 
-//////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// using System;
+
+// public class Kata
+// {
+//   public static string Smash(string[] words)
+//   {
+//     return string.Join(" ", words);
+//   }
+// }
+// //////////////////////////////////////////////////////////////////////////////////////////////////////
+// Rock Paper Scissors
+// Let's play! You have to return which player won! In case of a draw return Draw!.
+
+// Examples(Input1, Input2 --> Output):
+
+// "scissors", "paper" --> "Player 1 won!"
+// "scissors", "rock" --> "Player 2 won!"
+// "paper", "paper" --> "Draw!"
+
+
+
+
+// using System;
+// using System.Collections.Generic;
+// using System.Linq;
+// using System.Text.RegularExpressions;
+
+// public class Kata
+// {
+//   public string Rps(string p1, string p2)
+//   {
+//     if (p1 == p2)
+//       return "Draw!";
+
+//     if (((p1 == "rock") && (p2 == "scissors")) ||
+//         ((p1 == "scissors") && (p2 == "paper")) ||
+//         ((p1 == "paper") && (p2 == "rock")))
+//     {
+//       return "Player 1 won!";
+//     }
+//     else
+//     {
+//       return "Player 2 won!";
+//     }
+//   }
+// }
+
+
 //////////////////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////////////////
